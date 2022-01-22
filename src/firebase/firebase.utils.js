3ea8ee9baf59fa6fbc17a38bfee90c21b88fe75d
Vivadhyay <vivekupadhyay.92@gmail.com>
2022-01-22 16:06:05 +0530

    added firebase functionality to fetch data, withspinner, redux thunk, and redux compose

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const config = {
   apiKey: "AIzaSyCm26rsOucbZXdEOJu59w2em7DQIxb3ubA",
@@ -9,7 +9,7 @@ const config = {
   storageBucket: "wardrobe-26a7d.appspot.com",
   messagingSenderId: "768429712284",
   appId: "1:768429712284:web:f4741f3961f64d67241e59",
-  measurementId: "G-2L24E5MFND"
+  measurementId: "G-2L24E5MFND",
 };
 
 firebase.initializeApp(config);
@@ -29,21 +29,54 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
-  
+
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
 // const middlewares = [logger];
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
@@ -15,4 +16,4 @@ export const store = createStore(rootReducer, applyMiddleware (...middlewares));
 
 export const persistor = persistStore(store); 
 
-// export default (store, persistor);
+export default (store, persistStore)
